30d70a73d08a0ef1d7635f0bc1c0be7179872f40
Pat Collins <padjo@mac.com>
2018-02-20 20:12:26 +0100

    file structure added

---
+++ intensive02/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Second from './components/02'; // Component with Props from Parent.
+import Third from './components/03';
+import Fourth from './components/04';
+import Fifth from './components/05';
+import Sixth from './components/06';
+import Seventh from './components/07';
+import Eighth from './components/08';
+import Ninth from './components/09';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Second blanket={'Blanky'} />
+        <Third />
+        <Fourth />
+        <Fifth />
+        <Sixth />
+        <Seventh />
+        <Eighth />
+        <Ninth />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ intensive02/src/components/09/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class ComponentName extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    componentDidMount() {
+    }
+    render() {
+        return (
+            <div className='greeting'>{}</div>
+        );
+    }
+}
